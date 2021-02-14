ad34ebf0640307ccc84d6d1c33c04a3f6ed8d461
Henry Jiang <h243jian@uwaterloo.ca>
2021-02-13 17:03:40 -0800

    Added API File

---
+++ source/api.h
@@ -0,0 +1,14 @@
+/***********************************************************************
+* FILENAME :        api.c   api.h
+*
+* DESCRIPTION :
+*       The main API file for interacting with the erosion simulator.
+*
+* PUBLIC FUNCTIONS :
+*          
+* NOTES :
+*
+* AUTHOR :    Henry Jiang         DATE :    Feb 13, 2021
+*/
+
+
